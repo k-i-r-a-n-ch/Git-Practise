7855573d965272835b1da32e313354c02d678e86
k-i-r-a-n-ch <kiran.162011@gmail.com>
2024-11-11 17:25:08 +0530

    inputs.sh

--- inputs.sh
+++ inputs.sh
@@ -2,6 +2,13 @@
 
 echo "Please enter your username::"
 
-read USERNAME
+read USERNAME #takes input from user
 
 echo "username entered is : $USERNAME"
+
+
+echo "Please enter your password::"
+
+read -s password
+
+echo "password entered is :: $password"
