6e43033a0990259f6895c5aec00e5ca851f70b8c
k-i-r-a-n-ch <kiran.162011@gmail.com>
2024-11-11 17:04:17 +0530

    variables

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=ramesh
+person2=suresh
+
+echo "$person1 :: Hi $person2, How are you?"
+echo "$person2 :: Hello $person1. i am fine" 
+echo "$person1 :: Hi $person2, How are you?"
+echo "$person2 :: Hello $person1. i am fine" 
