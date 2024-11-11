1e843046bbd6642bbe937d837b86fff95de537c1
k-i-r-a-n-ch <kiran.162011@gmail.com>
2024-11-11 16:55:10 +0530

    coversation

---
+++ conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh :: Hi Suresh, How are you?"
+echo "Suresh :: Hello Ramesh. i am fine" 
+echo "Ramesh :: Hi Suresh, How are you?"
+echo "Suresh :: Hello Ramesh. i am fine" 
