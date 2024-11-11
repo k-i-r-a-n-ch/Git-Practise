cde5088bc95b155ee1888915c38f03e5db2defff
k-i-r-a-n-ch <kiran.162011@gmail.com>
2024-11-11 17:35:30 +0530

    inputs.sh

--- arrays.sh
+++ arrays.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 fruits=("APPLE" "KIWI" "Orange")
 
-echo "first fruit is: $fruits[0]"
-echo "first fruit is: $fruits[1]"
-echo "first fruit is: $fruits[2]"
+echo "first fruit is: ${fruits[0]}"
+echo "first fruit is: ${fruits[1]}"
+echo "first fruit is: ${fruits[2]}"
