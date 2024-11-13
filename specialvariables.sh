0f83bde1445d8c8497b75ac0212fb63fe344be07
k-i-r-a-n-ch <kiran.162011@gmail.com>
2024-11-13 15:50:28 +0530

    pushing

--- specialvariables.sh
+++ specialvariables.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo "All variables passed to the script :$@"
+echo "All variables passed to the script :$@"  #i want al variables passed to scrfipt
+
+
+echo "number of  variables passed to the script :$#"
+
+
+
+echo "script name :$0"
+
+
+
+echo "current working directory :$PWD"
+
+
+echo "Home directory of current user :$HOME"
