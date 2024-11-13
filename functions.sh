b9139955521e8f1c41ad197cda01c26d0e742994
k-i-r-a-n-ch <kiran.162011@gmail.com>
2024-11-13 23:39:11 +0530

    pushing

--- functions.sh
+++ functions.sh
@@ -2,6 +2,10 @@
 
 userid=$(id -u)
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 
 CHECK_ROOT(){
     if [ $userid -ne 0 ]
@@ -14,10 +18,10 @@ CHECK_ROOT(){
 VALIDATE(){
      if [ $1 -ne 0 ]
      then  
-         echo "$2 is ... failed"
+         echo -e "$2 is ...$R failed $N"
          exit 1
      else 
-         echo "$2 is .... success"
+         echo -e "$2 is ....$G success $N"
      fi
 }
 
