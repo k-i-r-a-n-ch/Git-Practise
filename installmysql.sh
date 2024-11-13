1c0f4c259f76ba221bc2cb2389da30e4437908d1
k-i-r-a-n-ch <kiran.162011@gmail.com>
2024-11-13 16:17:59 +0530

    pushing

--- installmysql.sh
+++ installmysql.sh
@@ -3,3 +3,11 @@
 userid=$(id -u)
 
 echo "user id is :$userid"
+
+if [ $userid -ne 0 ]
+then
+   echo "please run this script through root priviliges"
+   exit 1     #shell script will come out of the program
+fi
+
+dnf install 
