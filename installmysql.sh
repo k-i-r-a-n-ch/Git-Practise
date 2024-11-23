3f3e93be29f4a87209e27efc4bb013d0bf129b71
k-i-r-a-n-ch <kiran.162011@gmail.com>
2024-11-23 16:47:48 +0530

    ...

--- installmysql.sh
+++ installmysql.sh
@@ -3,6 +3,13 @@
 
 USERID=$(id -u)
 
-echo "USERID is $USERID"
+#echo "USERID is $USERID"
+
+if[ $USERID -ne 0 ]
+then 
+    echo "Please Run This Script Through Root Priviliges"
+fi
+
+dnf install git -y
 
 
