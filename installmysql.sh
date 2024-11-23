820ff654c8b3d0b405999d39af9a6df1cb5fa608
k-i-r-a-n-ch <kiran.162011@gmail.com>
2024-11-23 18:26:48 +0530

    ...

--- installmysql.sh
+++ installmysql.sh
@@ -3,6 +3,15 @@
 
 USERID=$(id -u)
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then    
+        echo "$2 is....FAILED"
+    else
+        echo "$2 is....SUCCESS"
+    fi
+}
+
 if [ $USERID -ne 0 ]
 then 
     echo "Please Run This Script Through Root Priviliges"
@@ -15,12 +24,7 @@ if [ $? -ne 0 ]
 then
     echo "Git is not installed, going to install it..."
     dnf install git -y
-    if [ $? -ne 0 ]
-    then  
-        echo "Git installation is not success....check it"
-    else
-       echo "Git installation is success..."
-    fi
+    VALIDATE $? "Installing GIT"
 else
     echo "Git is Already Installed..."
 fi
@@ -32,13 +36,7 @@ if [ $? -ne 0 ]
 then
     echo "MySql is not installed...going to install it"
     dnf install mysql -y
-    if [ $? -ne 0 ]
-    then 
-        echo "MYsql installation is failure...please check"
-        exit 1
-    else
-        echo "mysql insatallation is success"
-    fi
+    VALIDATE $? "Installing MYSQL"
 else
     echo "MYSQL is already installed.... nothing to do"
 fi
