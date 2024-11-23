29e72e2df44a741529e9682beb99c0c15a370bda
k-i-r-a-n-ch <kiran.162011@gmail.com>
2024-11-23 17:52:10 +0530

    ...

--- installmysql.sh
+++ installmysql.sh
@@ -15,8 +15,30 @@ if [ $? -ne 0 ]
 then
     echo "Git is not installed, going to install it..."
     dnf install git -y
+    if [ $? -ne 0 ]
+    then  
+        echo "Git installation is not success....check it"
+    else
+       echo "Git installation is success..."
+    fi
 else
     echo "Git is Already Installed..."
 fi
 
 
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "MySql is not installed...going to install it"
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then 
+        echo "MYsql installation is failure...please check"
+        exit 1
+    else
+        echo "mysql insatallation is success"
+    fi
+else
+    echo "MYSQL is already installed.... nothing to do"
+fi
